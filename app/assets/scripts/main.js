5f7a32145ea0c33f9d4210f970cdd72e7022acee
David Ramos <dave@imaginaryterrain.com>
2014-07-29 14:45:20 -0400

    fix script build process

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,9 +1,16 @@
 console.log('\'Allo \'Allo!');
 
 
-/* nav menu */
 
+/* nav menu */
 $("#show-mobile-nav").click(function() {
 		$(".nav-mobile-hidden").toggle(200);
 });
 
+
+
+// masonry for layout
+var container = document.querySelector('.masonry-layout');
+var msnry = new Masonry( container, {
+  itemSelector: 'section'
+});
